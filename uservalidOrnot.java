bc642f3cdb710ffadceca8ca28c4f4b8154a3e46
Praveen Ganesh <107360402+praveenganesh123@users.noreply.github.com>
2023-02-08 09:55:53 +0530

    Add files via upload

---
+++ uservalidOrnot.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+public class uservalidOrnot
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+
+System.out.println("user name=");
+
+String u1=sc.nextLine();
+
+System.out.println("user name2=");
+
+String u2=sc.nextLine();
+
+if(u1 == u1)
+{
+System.out.println("Valid user name");
+}
+
+else{
+System.out.println("INVALID!!");
+}
+}
+}
+
+
+
